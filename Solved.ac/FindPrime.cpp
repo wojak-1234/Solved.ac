5e94f7415fd54dcddc2a2ef9ec269d9f4e33ae74
wojak-1234 <wojak-@naver.com>
2025-02-18 13:49:54 +0900

    초기 커밋.

---
+++ Solved.ac/FindPrime.cpp
@@ -0,0 +1,30 @@
+﻿#include<stdio.h>
+#include<string.h>
+int notprime = 0;
+void isprime(int k)
+{
+    for (int p = 2; p < k; p++)
+    {
+        if (k % p == 0)
+        {
+            notprime += 1;
+            break;
+        }
+    }
+}
+int main()
+{
+    int a, b;
+    scanf_s("%d", &a);
+    for (int k = 0; k < a; k++)
+    {
+        scanf_s("%d", &b);
+        if (b == 1)
+        {
+            notprime += 1;
+        }
+        else
+            isprime(b);
+    }
+    printf("%d", a - notprime);
+}
